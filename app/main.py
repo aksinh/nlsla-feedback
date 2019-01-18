8eb383f5223a90c472b81ca3edde0abb1bccf2e1
lw453 <lw453@cornell.edu>
2019-01-18 17:55:21 -0500

    Add placeholder to receive data

--- app/main.py
+++ app/main.py
@@ -1,11 +1,15 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 ENTRY_POINT = ""
 
-@app.route(ENTRY_POINT + '/', methods=['GET'])
+@app.route(ENTRY_POINT + '/', methods=['GET','POST'])
 def index():
-  return render_template('index.html')
+    if request.method == 'GET':
+        return render_template('index.html')
+    else:
+        #  Replace to receive data with request.form.get()
+        return
 
 if __name__ == "__main__":
     app.run(debug=True)
