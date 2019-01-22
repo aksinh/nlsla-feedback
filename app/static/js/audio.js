1c7abcaf5a67c87a8e12014574a424989f76305c
Stephanie Shum <ss2972@cornell.edu>
2019-01-21 21:39:25 -0500

    Finished audio toggle

--- app/static/js/audio.js
+++ app/static/js/audio.js
@@ -1,14 +1,75 @@
+
+
 $(document).ready(function() {
+
   var audioOn = true;
 	$("#read-icon").click(function() {
     if (audioOn) {
-      $("#read-on").addClass("hidden");
-      $("#read-off").removeClass("hidden");
+      document.getElementById("read-on").innerHTML = "Off";
       audioOn = false;
+      window.speechSynthesis.cancel();
+
     } else {
-      $("#read-off").addClass("hidden");
-      $("#read-on").removeClass("hidden");
+      document.getElementById("read-on").innerHTML = "On";
       audioOn = true;
+      var msg = new SpeechSynthesisUtterance($('.pt-page-1').text());
+      msg.volume = 1;
+      window.speechSynthesis.speak(msg);
+
     }
 	});
+
+
+  $('.pt-page-1').mouseenter(function(){
+    window.speechSynthesis.cancel();
+    var msg = new SpeechSynthesisUtterance($('.pt-page-1').text());
+    if (audioOn) {
+      msg.volume = 1;
+    } else {
+      msg.volume = 0;
+    }
+    window.speechSynthesis.speak(msg);
+  })
+  $('.pt-page-2').mouseenter(function(){
+    window.speechSynthesis.cancel();
+    var msg = new SpeechSynthesisUtterance($('.pt-page-2').text());
+    if (audioOn) {
+      msg.volume = 1;
+    } else {
+      msg.volume = 0;
+    }
+    window.speechSynthesis.speak(msg);
+  })
+  $('.pt-page-3').mouseenter(function(){
+    window.speechSynthesis.cancel();
+    var msg = new SpeechSynthesisUtterance($('.pt-page-3').text());
+    if (audioOn) {
+      msg.volume = 1;
+    } else {
+      msg.volume = 0;
+    }
+    window.speechSynthesis.speak(msg);
+  })
+  $('.pt-page-4').mouseenter(function(){
+    window.speechSynthesis.cancel();
+    var msg = new SpeechSynthesisUtterance($('.pt-page-4').text());
+    if (audioOn) {
+      msg.volume = 1;
+    } else {
+      msg.volume = 0;
+    }
+    window.speechSynthesis.speak(msg);
+  })
+  $('.pt-page-5').mouseenter(function(){
+    window.speechSynthesis.cancel();
+    var msg = new SpeechSynthesisUtterance($('.pt-page-5').text());
+    if (audioOn) {
+      msg.volume = 1;
+    } else {
+      msg.volume = 0;
+    }
+    window.speechSynthesis.speak(msg);
+  })
+
+
 });
