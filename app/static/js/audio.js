6f8bdc22a7951eace8a0c7093d3fab1520465f12
Stephanie Shum <ss2972@cornell.edu>
2019-01-21 23:31:17 -0500

    Adjust audio toggle, add highlighting

--- app/static/js/audio.js
+++ app/static/js/audio.js
@@ -5,71 +5,85 @@ $(document).ready(function() {
   var audioOn = true;
 	$("#read-icon").click(function() {
     if (audioOn) {
-      document.getElementById("read-on").innerHTML = "Off";
-      audioOn = false;
       window.speechSynthesis.cancel();
+      document.getElementById("read-on").innerHTML = "Off";
+      var msg = new SpeechSynthesisUtterance("Off");
+      audioOn = false;
+      window.speechSynthesis.speak(msg);
 
     } else {
+      window.speechSynthesis.cancel();
       document.getElementById("read-on").innerHTML = "On";
+      var msg = new SpeechSynthesisUtterance("On");
       audioOn = true;
-      var msg = new SpeechSynthesisUtterance($('.pt-page-1').text());
-      msg.volume = 1;
       window.speechSynthesis.speak(msg);
 
     }
 	});
 
+  $('.hasText').mouseenter(function(){
+    window.speechSynthesis.cancel();
+    var msg = new SpeechSynthesisUtterance(this.textContent);
+    if (audioOn) window.speechSynthesis.speak(msg);
+    this.style.backgroundColor = "yellow";
+  })
 
-  $('.pt-page-1').mouseenter(function(){
-    window.speechSynthesis.cancel();
-    var msg = new SpeechSynthesisUtterance($('.pt-page-1').text());
-    if (audioOn) {
-      msg.volume = 1;
-    } else {
-      msg.volume = 0;
-    }
-    window.speechSynthesis.speak(msg);
+  $('.hasText').mouseleave(function(){
+    this.style.backgroundColor = "transparent";
+
   })
-  $('.pt-page-2').mouseenter(function(){
-    window.speechSynthesis.cancel();
-    var msg = new SpeechSynthesisUtterance($('.pt-page-2').text());
-    if (audioOn) {
-      msg.volume = 1;
-    } else {
-      msg.volume = 0;
-    }
-    window.speechSynthesis.speak(msg);
-  })
-  $('.pt-page-3').mouseenter(function(){
-    window.speechSynthesis.cancel();
-    var msg = new SpeechSynthesisUtterance($('.pt-page-3').text());
-    if (audioOn) {
-      msg.volume = 1;
-    } else {
-      msg.volume = 0;
-    }
-    window.speechSynthesis.speak(msg);
-  })
-  $('.pt-page-4').mouseenter(function(){
-    window.speechSynthesis.cancel();
-    var msg = new SpeechSynthesisUtterance($('.pt-page-4').text());
-    if (audioOn) {
-      msg.volume = 1;
-    } else {
-      msg.volume = 0;
-    }
-    window.speechSynthesis.speak(msg);
-  })
-  $('.pt-page-5').mouseenter(function(){
-    window.speechSynthesis.cancel();
-    var msg = new SpeechSynthesisUtterance($('.pt-page-5').text());
-    if (audioOn) {
-      msg.volume = 1;
-    } else {
-      msg.volume = 0;
-    }
-    window.speechSynthesis.speak(msg);
-  })
+
+
+  // $('.pt-page-1').mouseenter(function(){
+  //   window.speechSynthesis.cancel();
+  //   var msg = new SpeechSynthesisUtterance($('.pt-page-1').text());
+  //   if (audioOn) {
+  //     msg.volume = 1;
+  //   } else {
+  //     msg.volume = 0;
+  //   }
+  //   window.speechSynthesis.speak(msg);
+  // })
+  // $('.pt-page-2').mouseenter(function(){
+  //   window.speechSynthesis.cancel();
+  //   var msg = new SpeechSynthesisUtterance($('.pt-page-2').text());
+  //   if (audioOn) {
+  //     msg.volume = 1;
+  //   } else {
+  //     msg.volume = 0;
+  //   }
+  //   window.speechSynthesis.speak(msg);
+  // })
+  // $('.pt-page-3').mouseenter(function(){
+  //   window.speechSynthesis.cancel();
+  //   var msg = new SpeechSynthesisUtterance($('.pt-page-3').text());
+  //   if (audioOn) {
+  //     msg.volume = 1;
+  //   } else {
+  //     msg.volume = 0;
+  //   }
+  //   window.speechSynthesis.speak(msg);
+  // })
+  // $('.pt-page-4').mouseenter(function(){
+  //   window.speechSynthesis.cancel();
+  //   var msg = new SpeechSynthesisUtterance($('.pt-page-4').text());
+  //   if (audioOn) {
+  //     msg.volume = 1;
+  //   } else {
+  //     msg.volume = 0;
+  //   }
+  //   window.speechSynthesis.speak(msg);
+  // })
+  // $('.pt-page-5').mouseenter(function(){
+  //   window.speechSynthesis.cancel();
+  //   var msg = new SpeechSynthesisUtterance($('.pt-page-5').text());
+  //   if (audioOn) {
+  //     msg.volume = 1;
+  //   } else {
+  //     msg.volume = 0;
+  //   }
+  //   window.speechSynthesis.speak(msg);
+  // })
 
 
 });
