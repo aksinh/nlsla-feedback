81dd4832e9a00c54ff3902fd3dee3cc0aff34f1b
nataly_r <nyr2@cornell.edu>
2019-04-25 17:42:22 -0400

    updated qs

--- app/static/js/pagetransitions.js
+++ app/static/js/pagetransitions.js
@@ -19,7 +19,7 @@ var PageTransitions = (function() {
 		animEndEventName = animEndEventNames[ Modernizr.prefixed( 'animation' ) ],
 		// support css animations
 		support = Modernizr.cssanimations;
-	
+
 	function init() {
 
 		$pages.each( function() {
@@ -58,7 +58,7 @@ var PageTransitions = (function() {
 		}
 
 		isAnimating = true;
-		
+
 		var $currPage = $pages.eq( current );
 
 		if(options.showPage){
@@ -129,7 +129,7 @@ var PageTransitions = (function() {
 
 	init();
 
-	return { 
+	return {
 		init : init,
 		nextPage : nextPage,
 	};
